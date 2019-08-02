88448fe8468dd19cdaace7cf01110f4435a5a5a9
Saurav <vermasaaab1@gmail.com>
2019-08-02 09:33:36 +0530

    Kadane Algorithm

--- sum_contiguous_subarray.c
+++ sum_contiguous_subarray.c
@@ -4,19 +4,27 @@ void contiguous_sum_array(int arr[],int size)
 {
 	int max=0,max_1=0;
 	int i;
+	int start,end;
 	for(i=0;i<size;i++)
 	{
 		max_1=max_1+arr[i];
 		if(max_1<0)
 		{
+			start=i+1;
 			max_1=0;
 		}
 		if(max<max_1)
 		{
+			end=i;
 			max=max_1;
 		}
 	}
-	printf("max sum is %d",max);
+	printf("max sum is %d\nMax Sub array is\n",max);
+	for(i=start;i<=end;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+
 }
 int main(int argc,const char* argv[])
 {
